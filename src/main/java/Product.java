99ac6bda34ec9fa295140ea31baeaac258a44e87
Marre24 <93831034+Marre24@users.noreply.github.com>
2024-10-14 14:41:24 +0200

    Add toString representation

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,18 +1,24 @@
 public class Product {
     private final String name;
-    private final float price;
+    private final float priceTag;
 
-    public Product (String name, float price){
+    public Product (String name, float priceTag){
         this.name = name;
-        this.price = price;
+        this.priceTag = priceTag;
     }
 
     public String getName() {
         return name;
     }
 
-    public float getPrice() {
-        return price;
+    public float getPriceTag() {
+        return priceTag;
     }
 
+    @Override
+    public String toString() {
+        return "Product: " +
+                 name +
+                ", Price tag: " + priceTag;
+    }
 }
--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -15,9 +15,16 @@ public class TestProduct {
     public void CreateClassInstance_PriceTag_ClassInstance() {
         float expectedPriceTag = 10;
         Product product = new Product("",expectedPriceTag);
-        assertEquals(expectedPriceTag, product.getPrice());
+        assertEquals(expectedPriceTag, product.getPriceTag());
     }
 
-
+    @Test
+    public void ToString_NameAndPrice_StringRepresentation() {
+        String productName = "Juice";
+        float priceTag = 15;
+        String expectedStringRepresentation = "Product: " + productName + ", Price tag: " + priceTag;
+        Product product = new Product(productName, priceTag);
+        assertEquals(expectedStringRepresentation, product.toString());
+    }
 
 }
