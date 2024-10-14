ec74c376bafcd6947501b8b167f5616a4e58028a
Marre24 <93831034+Marre24@users.noreply.github.com>
2024-10-14 14:18:31 +0200

    Add price and name in Product and tested it

--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestProduct {
+
+    @Test
+    public void CreateClassInstance_ProductName_ClassInstance() {
+        String expectedProductName = "Milk";
+        Product product = new Product(expectedProductName, 0);
+        assertEquals(expectedProductName, product.getName());
+    }
+
+    @Test
+    public void CreateClassInstance_PriceTag_ClassInstance() {
+        float expectedPriceTag = 10;
+        Product product = new Product("",expectedPriceTag);
+        assertEquals(expectedPriceTag, product.getPrice());
+    }
+
+
+
 }
