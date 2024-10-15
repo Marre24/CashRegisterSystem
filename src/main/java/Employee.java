1db0404870170d55f3a30c36273bddd5a3f9f26d
Marre24 <93831034+Marre24@users.noreply.github.com>
2024-10-15 14:45:53 +0200

    Add Employee logic

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,32 @@
-public class Employee {
+public class Employee extends Person{
+
+    private String title = "NO TITLE";
+    private int monthlySalary = 0;
+
+    public Employee(String firstName, String surName, String socialSecurityNr, String phoneNumber, String emailAdress, String homeAdress) {
+        super(firstName, surName, socialSecurityNr, phoneNumber, emailAdress, homeAdress);
+    }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
+
+    public void setMonthlySalary(int monthlySalary) {
+        if (monthlySalary < 0)
+            throw new IllegalArgumentException("Monthly salary should not be set to a negative number");
+        this.monthlySalary = monthlySalary;
+    }
+
+    public int getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- src/test/java/TestEmployee.java
+++ src/test/java/TestEmployee.java
@@ -1,2 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TestEmployee {
+
+
+    final private String firstName = "FirstName";
+    final private String surName = "SurName";
+    final private String phoneNumber = "111111111111";
+    final private String emailAdress = "Email@Adress.com";
+    final private String homeAdress = "Adress1";
+    final private String socialSecurityNr = "11112233-4444";
+
+    final String noTitle = "NO TITLE";
+    final String title = "TITLE";
+
+    @Test
+    void Constructor_ValidArguments_ValidId(){
+        Employee employee = new Employee(firstName, surName, socialSecurityNr, phoneNumber, emailAdress, homeAdress);
+        assertEquals(employee.getFullName(), employee.toString());
+    }
+
+    @Test
+    void TitleGetterSetter_OtherTitle_ChangesTitle(){
+        Employee employee = new Employee(firstName, surName, socialSecurityNr, phoneNumber, emailAdress, homeAdress);
+
+        assertEquals(noTitle, employee.getTitle());
+
+        employee.setTitle(title);
+
+        assertEquals(title, employee.getTitle());
+    }
+
+    @Test
+    void SalaryGetterSetter_PositiveValue_ChangesSalary(){
+        Employee employee = new Employee(firstName, surName, socialSecurityNr, phoneNumber, emailAdress, homeAdress);
+
+        assertEquals(0, employee.getMonthlySalary());
+
+        employee.setMonthlySalary(10);
+
+        assertEquals(10, employee.getMonthlySalary());
+    }
+
+    @Test
+    void SalarySetter_NegativeValue_ExceptionThrown(){
+        Employee employee = new Employee(firstName, surName, socialSecurityNr, phoneNumber, emailAdress, homeAdress);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> { employee.setMonthlySalary(-1); },
+                "Negative parameter value should throw exception");
+    }
+
 }
