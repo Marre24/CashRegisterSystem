313e068698c3af69b4af8751b2dc5bf029e2fbd3
Alex <alexrosenk@hotmail.se>
2024-10-15 13:26:44 +0200

    rework TestProductScanner

--- src/test/java/TestProductScanner.java
+++ src/test/java/TestProductScanner.java
@@ -16,7 +16,7 @@ public class TestProductScanner {
         Product expectedProductScanned = new Product("someProduct", money);
         productScanner.scanProduct(expectedProductScanned);
 
-        var actual = productScanner.getScannedItems().getFirst();
+        Product actual = productScanner.getScannedItems().getFirst();
 
         assertEquals(actual, expectedProductScanned);
     }
@@ -30,6 +30,3 @@ public class TestProductScanner {
 
 
 }
-
-// ProductScanner should hold the scanned data in its own data structure, and when a customer is done, ps.writeReceipt() creates
-// a new Receipt with the held data, and ps clears its data.
