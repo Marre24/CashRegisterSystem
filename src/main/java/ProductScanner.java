e4b66d18f911a5223e908e3736f553eb39c6f64a
Alex <alexrosenk@hotmail.se>
2024-10-15 10:53:28 +0200

    Added ProductScanner

--- src/main/java/ProductScanner.java
+++ src/main/java/ProductScanner.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductScanner {
+
+    private final List<Product> products;
+
+    public ProductScanner(){
+        products = new ArrayList<>();
+    }
+
+
+    public void scanProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getScannedItems(){
+        return products;
+    }
 }
--- src/test/java/TestProductScanner.java
+++ src/test/java/TestProductScanner.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestProductScanner {
+
+
+    private final Money money = new Money(Currency.SEK, new BigDecimal(10));
+    // actual, expected
+    // test method for scanning product. Assumes Receipt class, and its constructor working
+    @Test
+    void productScannerIsUpdatedOnProductScanned(){
+        ProductScanner productScanner = new ProductScanner();
+        Product expectedProductScanned = new Product("someProduct", money);
+        productScanner.scanProduct(expectedProductScanned);
+
+        var actual = productScanner.getScannedItems().getFirst();
+
+        assertEquals(actual, expectedProductScanned);
+    }
 }
+
+// ProductScanner should hold the scanned data in its own data structure, and when a customer is done, ps.writeReceipt() creates
+// a new Receipt with the held data, and ps clears its data.
