93e9013661d05e69753a9917ec0eee9e60bb3fd7
Alex <alexrosenk@hotmail.se>
2024-10-21 11:18:05 +0200

    Added comments about Purchase functionality, renamed Purchase to TestPurchase

--- src/main/java/DebitCard.java
+++ src/main/java/DebitCard.java
@@ -1,3 +1,5 @@
+/* Data class */
+
 public class DebitCard {
 
     final private String bank;
@@ -5,12 +7,14 @@ public class DebitCard {
     final private String expirationDate;
     final private int csc;
     final private Person owner;
+    private long balance;
 
-    public DebitCard(Person owner, String bank, String PAN, String expirationDate, int csc){
+    public DebitCard(Person owner, String bank, String PAN, String expirationDate, int csc, long balance){
         this.owner = owner;
         this.bank = bank;
         this.PAN = PAN;
         this.expirationDate = expirationDate;
         this.csc = csc;
+        this.balance = balance;
     }
 }
--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -1,2 +1,15 @@
 public class Purchase {
+
+
 }
+
+
+// When customer blippar kreditkort, then Purchase is created
+// Has  reference to order, and a debit card.
+// creates/generates receipt for this purchase at end of purchase
+// "Is a finished order"
+
+// Functionality to be tested: Withdraw order "total amount" from DebitCard ??
+// createReceipt(), creates the receipt for this purchase, with info sent AND debitcard info
+// tipping?
+// Debit type or credit type?
