c7a919411095a7e1546f505bd67271a9812e902b
Otto Westling <jovw@hotmail.com>
2024-10-24 15:58:31 +0200

    Updated ProductScanner with enhances functionality and added some tests in TestProductScanner, also updated interaction between employee, ProductScanner and Order.
    //Otto och Ludde

--- src/main/java/ProductScanner.java
+++ src/main/java/ProductScanner.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ProductScanner {
-    private Order activeOrder = null;
+
+    private static ArrayList<Order> ordersOnHold = new ArrayList<>();
+    private Order activeOrder;
+    private Employee activeEmployee;
+
+    public ProductScanner(Employee activeEmployee) {
+        this.activeEmployee = activeEmployee;
+    }
 
     public void scanProduct(ProductType productType) {
         activeOrder.addProduct(productType);
@@ -13,11 +25,39 @@ public class ProductScanner {
         this.activeOrder = order;
     }
 
-    public Order getOrder(){
-        return activeOrder;
+    public Order getActiveOrder(){
+        if (hasActiveOrder())
+            return activeOrder;
+        else return null;
     } //Check if null
+
+    public Employee getActiveEmployee() {
+        return activeEmployee;
+    }
 
     public boolean containsProduct(ProductType p){
         return activeOrder.containsProduct(p);
+    }
+
+    public void startNewOrder(){
+        activeOrder = new Order(activeEmployee);
+    }
+
+    public void setActiveOrderOnHold(){
+        ordersOnHold.add(activeOrder);
+        activeOrder = null;
+    }
+
+    public ArrayList<Order> getOrdersOnHold(){
+        return ordersOnHold;
+    }
+
+    public Order getOrderOnHold(String s){
+        for (Order order : ordersOnHold) {
+            if (order.getId().equals(s)){
+                return order;
+            }
+        }
+        return null;
     }
 }
