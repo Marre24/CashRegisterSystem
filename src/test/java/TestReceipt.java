504fc6ebf6ad883cf40d9d03655c957e8b3c4845
maxi <maxi.ellnestam@gmail.com>
2024-10-29 19:39:56 +0100

    Small Refactor to TestReceipt

--- src/test/java/TestReceipt.java
+++ src/test/java/TestReceipt.java
@@ -15,27 +15,36 @@ public class TestReceipt {
     void PrintReceipt_ReceiptPrintsToFile_(){
         Employee employee = new Employee("FirstName","SurName","A","A","A","A");
         Person person = new Person("B","B","B","B","B","B");
+        PaymentCard paymentCard = new DebitCard(person, "b","1111-1111-1111-1111","1",1,0);
         ProductScanner productScanner = new ProductScanner();
+
         productScanner.setLoggedInEmployee(employee);
         productScanner.startNewOrder();
         productScanner.scanProduct(product);
         productScanner.scanProduct(product);
         productScanner.scanProduct(product2);
+
         Order order = productScanner.getActiveOrder();
-        String fileDirectory = "Receipts/" + order.getId() + ".txt";
-        PaymentCard paymentCard = new DebitCard(person, "b","1111-1111-1111-1111","1",1,0);
         Purchase purchase = new Purchase(order);
+
+        String fileDirectory = "Receipts/" + order.getId() + ".txt";
+
         Receipt.printReceipt(purchase, paymentCard);
+
         try{
             FileReader fileReader = new FileReader(fileDirectory);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             StringBuilder s = new StringBuilder();
-            String st = "";
-            while((st = bufferedReader.readLine()) != null){
-                 s.append(st + "\n");
+            String stringFromFile;
+
+            while((stringFromFile = bufferedReader.readLine()) != null){
+                 s.append(stringFromFile).append("\n");
             }
-            st = s.substring(0, s.length()-1);
-            assertEquals("%s\n\n%s\n\n%s\n\nPurchase id: %s".formatted(testCompany, testReceiptHeader, purchase.toString(), order.getId()), st.toString());
+
+            stringFromFile = s.substring(0, s.length()-1);
+            String expected = "%s\n\n%s\n\n%s\n%s\n\nPurchase id: %s".formatted(testCompany, testReceiptHeader, purchase.toString(), paymentCard.toString(), order.getId());
+
+            assertEquals(expected, stringFromFile);
         } catch (IOException e){
             e.printStackTrace();
         }
