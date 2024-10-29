d40b38b5f5c8455f380fb093f08b479e48bd777c
Alex <alexrosenk@hotmail.se>
2024-10-29 13:22:14 +0100

    Refactored TestDebitCard & TestCreditCard

--- src/test/java/TestDebitCard.java
+++ src/test/java/TestDebitCard.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TestDebitCard {
 
     private static final int BALANCE = 100;
+    private static final int MID_BALANCE = 50;
     private static final int LOW_BALANCE = 10;
 
     private static final String BANK_NAME = "Nordea";
@@ -14,6 +15,12 @@ public class TestDebitCard {
     private static final int CSC = 542;
 
     private final Product banana = new Product("Banana", 50, Producer.Arla);
+
+    @Test
+    void GetBalance_OnCreate_ReturnBalance(){
+        DebitCard card = new DebitCard(null, BANK_NAME, PAN, EXPIRATION_DATE, CSC, MID_BALANCE);
+        assertEquals(MID_BALANCE, card.getBalance());
+    }
 
     @Test
     void Pay_PriceGreaterThanBalance_ThrowsException(){
@@ -39,6 +46,18 @@ public class TestDebitCard {
         card.pay(order.getTotalPrice());
 
         assertEquals(BALANCE - banana.getPrice(), card.getBalance());
+    }
+
+    @Test
+    void Pay_PriceEqualToBalance_PriceDecreases(){
+        DebitCard card = new DebitCard(null, BANK_NAME, PAN, EXPIRATION_DATE, CSC, MID_BALANCE);
+        Order order = new Order(null);
+
+        order.addProduct(banana);
+
+        card.pay(order.getTotalPrice());
+
+        assertEquals(MID_BALANCE - banana.getPrice(), card.getBalance());
     }
 
 }
